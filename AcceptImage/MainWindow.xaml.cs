193f8dcdfe20c7a079d60cd4320c6dd63244693d
XSbzi_uh51 <XSbzi_uh51@itstep.edu.az>
2023-07-27 22:52:26 +0400

    Add project files.

---
+++ AcceptImage/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AcceptImage
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var ipAddress = IPAddress.Parse("192.168.31.90");
+            var port = 80;
+
+            using (var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+            {
+                var ep = new IPEndPoint(ipAddress, port);
+                socket.Bind(ep);
+                socket.Listen(10);
+               
+
+                while (true)
+                {
+                    var client = socket.Accept();
+                    Task.Run(() =>
+                    {
+                        userName.Content = client.RemoteEndPoint;
+
+                        var length = 0;
+                        var bytes = new byte[1024];
+
+                        do
+                        {
+                            length = client.Receive(bytes);
+                            var msg = Encoding.UTF8.GetString(bytes, 0, length);
+                        } while (true);
+
+                    });
+                }
+            }
+
+        }
+    }
+}
